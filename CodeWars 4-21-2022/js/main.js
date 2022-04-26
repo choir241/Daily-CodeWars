3ea86364d4ce91f0573414adbd2fe5ba36dba868
choir27 <richardchoi54@gmail.com>
2022-04-26 11:02:49 -0400

    put 4/26/22 in 4/21/22 by accident

--- CodeWars 4-21-2022/js/main.js
+++ CodeWars 4-21-2022/js/main.js
@@ -1,27 +1,34 @@
-// Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
+// Clock shows h hours, m minutes and s seconds after midnight.
 
+// Your task is to write a function which returns the time since midnight in milliseconds.
 // Examples
 // Input: [1, 5.2, 4, 0, -1]
 // Output: 9.2
 
+// Example:
+// h = 0
+// m = 1
+// s = 1
 // Input: []
 // Output: 0
 
+// result = 61000
+// Input constraints:
 // Input: [-2.398]
 // Output: -2.398
 
-// Assumptions
-// You can assume that you are only given numbers.
-// You cannot assume the size of the array.
-// You can assume that you do get an array and if the array is empty, return 0.
-// What We're Testing
-// We're testing basic loops and math operations. This is for beginners who are just learning loops and math operations.
-// Advanced users may find this extremely easy and can easily write this in one line.
+// 0 <= h <= 23
+// 0 <= m <= 59
+// 0 <= s <= 59
 
+function past(h, m, s){
+    return ((h*3600)+(m*60)+s)*1000;
+  }
 
-function sum (numbers) {
-  return numbers.length < 1? 0 : numbers.reduce((a,b)=>a+b,0)
-  };
-
-
-  let sum = numbers => numbers.length < 1? 0 : numbers.reduce((a,b)=>a+b,0)
+  function past(h, m, s){
+    var miliseconds = 0;
+    miliseconds = miliseconds + s * 1000;
+    miliseconds = miliseconds + m * 60000;
+    miliseconds = miliseconds + h * 3600000;
+    return miliseconds;
+  } 
