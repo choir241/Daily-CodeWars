3c98d0224221dd1ec9ef0eb1a166a9b3c59c8833
choir27 <richardchoi54@gmail.com>
2022-05-07 15:02:28 -0400

    finished 5/7/22 codewars problem

--- CodeWars 5-3-2022/js/main.js
+++ CodeWars 5-3-2022/js/main.js
@@ -1,12 +1,6 @@
-// Make a program that filters a list of strings and returns a list with only your friends name in it.
+// Complete the solution so that it reverses the string passed into it.
 
-// If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
 
-// Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
-
-// i.e.
-
-// friend ["Ryan", "Kieran", "Mark"] `shouldBe` ["Ryan", "Mark"]
-// Note: keep the original order of the names in the output.
-
-let friend = friends => friends.filter(ele=>ele.length===4)
+let solution = str => str.split('').reverse().join('')
