9cb3669e09cbe0ffe21c3f6ab5e5f6692a7b0f7c
choir27 <richardchoi54@gmail.com>
2022-05-16 12:49:48 -0400

    finished 5/14/2022, 5/15/2022, and 5/16/2022 codewar problems

---
+++ May 2022/CodeWars 5-16-2022/js/main.js
@@ -0,0 +1,9 @@
+// Simple, remove the spaces from the string, then return the resultant string.
+
+let noSpace = x => x.split(' ').join('')
+
+//string sentence
+//returning a string with no spaces
+//noSpace('Shuten Douji') -> 'ShutenDouji'
+//noSpace("El i z a beth") -> 'Elizabeth'
+//split(' ') join('')
