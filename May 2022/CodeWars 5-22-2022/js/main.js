efe956292d9f4f09e43c2a5574d8073131319b3e
choir27 <richardchoi54@gmail.com>
2022-05-22 13:01:41 -0700

    finished codewar problem 5/22/2022

---
+++ May 2022/CodeWars 5-22-2022/js/main.js
@@ -0,0 +1,10 @@
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+let even_or_odd = number => number % 2 == 0 ? 'Even' : 'Odd' 
+
+//integer positive/negative
+//return a string, even for even numbers, odd for odd numbers
+//even_or_odd(2) even
+//even_or_odd(0) even
+//even_or_odd(43) odd
+//number % 2 == 0 ? 'even' : 'odd' 
