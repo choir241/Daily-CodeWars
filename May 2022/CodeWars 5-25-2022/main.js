a5dc58b110612ccc2faf695afce4730c8ac3924b
choir27 <richardchoi54@gmail.com>
2022-05-25 20:27:33 -0400

    finished codewars 5/25/2022

---
+++ May 2022/CodeWars 5-25-2022/main.js
@@ -0,0 +1,7 @@
+let smash = words => words.join(' ').trim()
+
+//array of words
+//return sentence
+//no space in beginning and end
+//['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
+//join(' ')
