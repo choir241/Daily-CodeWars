9449a0040b8d65db97c78cc1d3be7fa5ed5f0eaa
choir27 <richardchoi54@gmail.com>
2022-06-01 12:49:18 -0400

    finished codewars 6-1-2022 problem

---
+++ June 2022/CodeWars 6-1-2022/js/main.js
@@ -0,0 +1,13 @@
+// Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height of the cuboid. Write a function to help Bob with this calculation.
+
+class Kata {
+    static getVolumeOfCuboid(length, width, height){
+      return length * width * height
+    }
+  }
+  
+  //have 3 positive integer values
+  //give back 1 positive integer value, volume of cube
+  //length * width * height
+  //getVolumeOfCuboid(1,2,2)
+  //getVolumeOfCuboid(6.3,2,5)
