c84c0692f90015f09b1ab81ae0a0233155211464
choir27 <richardchoi54@gmail.com>
2022-06-16 13:56:28 -0400

    finished CodeWars problem 6-16-2022

---
+++ June 2022/CodeWars 6-16-2022/js/main.js
@@ -0,0 +1,11 @@
+// Implement a function which multiplies two numbers.
+
+// Write here your multiply-function
+
+let multiply = (n1,n2) => n1 * n2
+
+//take 2 integers
+//return 1 integer, the product of the 2 integers
+//multiply(1,2) 2
+//multiply(0,10) 0
+// *
