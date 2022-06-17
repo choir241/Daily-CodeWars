173d424aefe63840070c9676ce9dde82ad8a5adc
choir27 <richardchoi54@gmail.com>
2022-06-17 13:03:43 -0400

    finished CodeWars 6-17-2022

---
+++ June 2022/CodeWars 6-17-2022/js/main.js
@@ -0,0 +1,18 @@
+// In this Kata we are passing a number (n) into a function.
+
+// Your code will determine if the number passed is even (or not).
+
+// The function needs to return either a true or false.
+
+// Numbers may be positive or negative, integers or floats.
+
+// Floats with decimal part non equal to zero are considered UNeven for this kata.
+
+let testEven = n => n % 2 === 0
+
+//taking in number
+//return boolean if even/not
+//console.log(testEven(2)) true
+//console.log(testEven(1.24)) false
+//console.log(testEven(-4)) true
+// n % 2 === 0 
