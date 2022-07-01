d6e3f30915e0dc2b06153ad6e7c1af3331a423fe
choir27 <richardchoi54@gmail.com>
2022-07-01 14:23:39 -0400

    finished CodeWars 7-1-2022

---
+++ July 2022/CodeWars 7-1-2022/js/main.js
@@ -0,0 +1,11 @@
+// Now you have to write a function that takes an argument and returns the square of it.
+
+// Write the "square"-function here
+
+let square  = n => Math.pow(n,2)
+
+//takes in an integer
+//return its square
+//square(3) => 9
+//square(20) => 400
+//Math.pow(n)
