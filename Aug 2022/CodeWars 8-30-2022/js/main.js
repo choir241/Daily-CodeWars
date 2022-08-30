7e9442c01023c573b4b29e3e6d252b107dd34ad7
Richard Choi <richardchoi54@gmail.com>
2022-08-30 15:05:30 -0400

    finished 8-30-2022 codewar problem

---
+++ Aug 2022/CodeWars 8-30-2022/js/main.js
@@ -0,0 +1,18 @@
+// Input: Array of elements
+
+// ["h","o","l","a"]
+
+// Output: String with comma delimited elements of the array in th same order.
+
+// "h,o,l,a"
+
+// Note: if this seems too simple for you try the next level
+
+function printArray(array){
+  return array.join(',')
+  }
+  
+  //take in a array of strings
+  //return the string with the commas in place
+  //['h','o','l','a']
+  //.join(',')
