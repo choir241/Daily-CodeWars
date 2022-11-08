eef56eed148b674f0177930a46e90889c74a9127
Richard Choi <richardchoi54@gmail.com>
2022-11-08 14:14:02 -0500

    finished 11-5-22 codewars array method problem

--- Nov 2022/CodeWars 11-5-2022/js/main.js
+++ Nov 2022/CodeWars 11-5-2022/js/main.js
@@ -55,3 +55,47 @@ function findMaxProduct(arr){
 //find the product of each sequence -> let product = 1, for loop (from 0 to array.length)
 //get the final array of all possible products -> let array = []
 //return the maximum possible product -> return Math.max()
+ 
+
+// When the sequence is 1 element apart: Product = 11 × 6 × (-2) × 0 × 5 × (-4) × 2 = 0.
+//  0 1 2 3 4 5 6 +1
+// When the sequence is 2 apart: Product = 6 × 0 × (-4) = 0.
+// 1 3 5 +2
+// When the sequence is 3 apart: Product = (-2) × (-4) = 8.
+// 2 5 8 +3
+// When the sequence is 4 apart: Product = 0
+// 3 7 +4
+// When the sequence is 5 apart: Product = 5
+// 4 9 +5
+// When the sequence is 6 apart: Product = -4.
+// 5 11 +6
+// When the sequence is 7 apart: Product = 2.
+// 6 13 +7
+
+console.log(findMaxProduct(  [11, 6, -2, 0, 5, -4, 2]))
+
+
+function findMaxProduct(arr){
+  let increment = 1
+  let array = []
+for(let i = 0; i < arr.length; i++){
+    let product = 1
+ //i is the first element of the sequence
+  for(let index = 0; index < arr.length;index+=increment){
+    if(arr[i+index]!==undefined){
+   product *= arr[i+index]
+    }
+  }
+  array.push(product)
+//   0 1 2 3 4 5 6
+//   1 3 5
+//   2 5
+//   3
+//   4
+//   5
+//   6
+//   7
+increment+=1
+}
+return Math.max(...array)
+}
