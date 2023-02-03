5e1c7cbae8a49dbe8e4d9b8ff283ba2e1d9caeaa
Richard Choi <richardchoi54@gmail.com>
2023-02-02 19:45:14 -0500

    finished 12-10-2022 codewars problem

---
+++ Dec 2022/12-10-2022 Codewars/js/main.js
@@ -0,0 +1,9 @@
+// Your task is to find the nearest square number, nearest_sq(n), of a positive integer n.
+
+// Goodluck :)
+
+// Check my other katas:
+
+function nearestSq(n){
+ return Math.pow(Math.round(Math.sqrt(n)), 2);
+}
